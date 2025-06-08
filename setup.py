d67ab573ccc294c31113ae3e01f6d76311861b94
Mahi <anol.mahi@gmail.com>
2025-06-08 11:39:19 +0600

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A capstone project of MLOPS',
+    author='Mahi Sarwar Anol',
+    license='',
+)
